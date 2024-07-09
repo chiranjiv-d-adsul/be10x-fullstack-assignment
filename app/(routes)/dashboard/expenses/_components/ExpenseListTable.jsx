51ce6f19fa037b011dec22d6243349259a6f24b2
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-07-10 01:34:18 +0530

    update

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -1,7 +1,7 @@
-import { Trash } from 'lucide-react';
-import React from 'react';
-import { eq } from 'drizzle-orm';
-import { toast } from 'sonner';
+import { Trash } from "lucide-react";
+import React from "react";
+import { eq } from "drizzle-orm";
+import { toast } from "sonner";
 import { getTableColumns, sql } from "drizzle-orm";
 import { db } from "../../../../../utils/dbConfig";
 import { Budgets } from "../../../../../utils/schema";
@@ -11,60 +11,71 @@ import { useState } from "react";
 import { index } from "drizzle-orm/mysql-core";
 import { Bug } from "lucide-react";
 import { desc } from "drizzle-orm";
+import { IndianRupee } from "lucide-react";
 
 function ExpenseListTable({ expensesList, refreshData }) {
-
-
-  const handleDelete = async(expense) => {
-    const result=await db.delete(Expenses).where(eq(Expenses.id,expense.id))
-    .returning();
-    if(result){
-      toast.success("Expense Deleted Successfully")
+  const handleDelete = async (expense) => {
+    const result = await db
+      .delete(Expenses)
+      .where(eq(Expenses.id, expense.id))
+      .returning();
+    if (result) {
+      toast.success("Expense Deleted Successfully");
       refreshData();
-
+    } else {
+      toast.error("Expense Deletion Failed");
     }
-    else{
-      toast.error("Expense Deletion Failed")
-    }
-  }
+  };
 
   return (
     <div className="overflow-x-auto  shadow-md">
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
-            <th scope="col" className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider">
+            <th
+              scope="col"
+              className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider"
+            >
               Name
             </th>
-            <th scope="col" className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider">
+            <th
+              scope="col"
+              className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider"
+            >
               Amount
             </th>
-            <th scope="col" className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider">
+            <th
+              scope="col"
+              className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider"
+            >
               Date
             </th>
-            <th scope="col" className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider">
+            <th
+              scope="col"
+              className="px-4 py-2 sm:px-6 sm:py-3 text-left text-xs font-bold text-black uppercase tracking-wider"
+            >
               Action
             </th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          {expensesList.map((expense,index) => (
+          {expensesList.map((expense, index) => (
             <tr key={expense.id}>
               <td className="px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                 {expense.name}
               </td>
-              <td className="px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm text-gray-500">
-                ${expense.amount}
+              <td className=" flex items-center px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm text-gray-500">
+              <IndianRupee  className="w-4 h-4" /> {expense.amount}
               </td>
               <td className="px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm text-gray-500">
                 {expense.createdAt}
               </td>
               <td className="px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm text-gray-500">
                 <Trash
-                onClick={() => handleDelete(expense)}
-                className="cursor-pointer hover:text-red-800 text-red-600" />
+                  onClick={() => handleDelete(expense)}
+                  className="cursor-pointer hover:text-red-800 text-red-600"
+                />
               </td>
-
             </tr>
           ))}
         </tbody>
